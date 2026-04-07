06d206214acf08baaa781f62caad5ddf3d53190d
Thomas Morrow <tmorrow@schmidtocean.org>
2026-04-07 10:42:23 -0400

    remove private functions from import - bad coding practice

--- src/soi_frame_extractor/metadata/__init__.py
+++ src/soi_frame_extractor/metadata/__init__.py
@@ -1,14 +1,10 @@
 """Metadata writers for EXIF, IPTC, XMP, iFDO, and BIIGLE formats."""
 
-from .apply_metadata import _build_exif, _build_iptc, _build_xmp
 from .assemble import assemble_biigle_records
 from .biigle import write_biigle_manifest
 from .ifdo import write_ifdo_manifest
 
 __all__ = [
-    "_build_exif",
-    "_build_iptc",
-    "_build_xmp",
     "assemble_biigle_records",
     "write_biigle_manifest",
     "write_ifdo_manifest",
